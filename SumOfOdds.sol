ff436366146b19fa2b06ac19d0c87656fd24df1e
mando-dev <mandocustom@gmail.com>
2021-10-05 19:12:59 +0900

    f c

---
+++ SumOfOdds.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.19; //first figure out the first num of each row in the pyramid
+contract Kata {
+  function rowSumOddNumbers(int n) public pure returns (int) {
+        // your code here
+        
+        int nRow = n * n * n;
+        return nRow;
+      }
+      }
